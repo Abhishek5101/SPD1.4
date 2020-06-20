52cf2aec18cac9d79bb88d2893c44463b2daf274
Abhishek5101 <miabhishekkulkarni@gmail.com>
2020-06-20 15:53:22 +0530

    lizz buzz

---
+++ fizzbuzz.py
@@ -0,0 +1,7 @@
+output = []
+for i in range(0, 6):
+	if i % 2 == 0:
+		output.append("Lizz")
+	else:
+		output.append(i)
+print(output)
--- interview.py
+++ interview.py
@@ -20,24 +20,35 @@ Output: false
 """
 
 
-def is_isomorphic(word1, word2):
-	if len(word1) != len(word2):
-		return False
-	ans_dict = {}
-	for index in range(len(word1)):
-		ans_dict[word1[index]] = []
+# def is_isomorphic(word1, word2):
+# 	if len(word1) != len(word2):
+# 		return False
+# 	ans_dict = {}
+# 	for index in range(len(word1)):
+# 		ans_dict[word1[index]] = []
+#
+# 	for index in range(len(word1)):
+# 		ans_dict[word1[index]] += [word2[index]]
+#
+# 	for k, v in ans_dict.items():
+# 		ans_dict[k] = set(v)
+#
+# 	for key, value in ans_dict.items():
+# 		if len(key) != len(value):
+# 			return False
+#
+# 	return True
 
-	for index in range(len(word1)):
-		ans_dict[word1[index]] += [word2[index]]
-	
-	for k, v in ans_dict.items():
-		ans_dict[k] = set(v)
-	
-	for key, value in ans_dict.items():
-		if len(key) != len(value):
-			return False
 
-	return True
+def fibonacci_1000():
+	fib = [0, 1]
+	keepgoing = True
+	while keepgoing:
+		new_term = fib[-1] + fib[-2]
+		fib.append(new_term)
+		str_new_term = str(new_term)
+		if len(str_new_term) > 1000:
+			return fib.index(new_term)
 
 
-print(is_isomorphic("foo", "gii"))
+print(fibonacci_1000())
